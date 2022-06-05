fdb4d6b6b5b4076c677b88aaa04c9abdaa9a0080
rohit gupta <r04itgupta@gmail.com>
2022-06-05 18:00:55 +0530

    basic setup of sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "./App.scss"
+
 function App() {
   return (
     <div className="App">
-      Syndicate
+      <h1>Hello Rohit</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus hic soluta non, modi, ratione tenetur maiores, rem repellendus cupiditate aliquid dolorum pariatur delectus dicta necessitatibus aspernatur officiis. Eum iure dicta mollitia adipisci, in deleniti laboriosam sapiente aliquam quas, eligendi ratione consectetur facilis provident voluptate nesciunt voluptatem modi quidem? Unde magnam nulla minima harum cum aut ipsum porro consequatur tenetur est earum ducimus placeat, architecto quibusdam quidem repellat voluptatem sint impedit nam id expedita eum, vitae nemo? Illo fuga dolorum ab inventore praesentium optio asperiores qui sunt iusto nihil excepturi expedita odio quas aspernatur nisi, doloremque ullam dolor, accusantium odit quae.</p>
     </div>
   );
 }
