05529efd5aed32cbd22ef830ff5c7b003e1a74b9
Rohit Gupta <lucidrohit@gmail.com>
2022-10-28 10:37:57 +0530

    improved responsiveness in navigation block

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,61 +1,57 @@
-import "./navBar.scss"
+import "./navBar.scss";
 
 import IconText from "../common/iconText/iconText";
 
-import about from "./icons/about.js"
-import community from "./icons/community.js"
-import home from "./icons/home.js"
-import team from "./icons/team.js"
-import events from "./icons/events.js"
+import about from "./icons/about.js";
+import community from "./icons/community.js";
+import home from "./icons/home.js";
+import team from "./icons/team.js";
+import events from "./icons/events.js";
 const navBar = [
-    {
-        icon: home,
-        text: 'Home',
-        path:'home'
+  {
+    icon: home,
+    text: "Home",
+    path: "",
+  },
+  {
+    icon: events,
+    text: "Events",
+    path: "events",
+  },
+  {
+    icon: about,
+    text: "About Us",
+    path: "about",
+  },
+  {
+    icon: community,
+    text: "Community",
+    path: "community",
+  },
+  {
+    icon: team,
+    text: "Team",
+    path: "team",
+  },
+];
 
-    },
-    {
-        icon: events,
-        text: 'Events',
-        path:'events'
-    },
-    {
-        icon: about,
-        text: 'About Us',
-        path:'about'
-    },
-    {
-        icon: community,
-        text: 'Community',
-        path:'community'
-    },
-    {
-        icon: team,
-        text: 'Team',
-        path:'team'
-    }
-]
-
-
-const NavBar = ({activeClass,setActiveClass}) => {
-
-    return (
-        <div className="navbar">
-            <div className="box--bg box ">
-                {navBar.map(({ icon, text, path }, index) => (
-                    <IconText
-                        icon={icon}
-                        text={text}
-                        path={path}
-                        key={index}
-                        setActiveClass={setActiveClass}
-                        activeClass={activeClass}
-                         />
-                ))
-                }
-            </div>
-        </div>
-    )
-}
+const NavBar = ({ activeClass, setActiveClass }) => {
+  return (
+    <div className="navbar sideBar__element">
+      <div className="box--bg box ">
+        {navBar.map(({ icon, text, path }, index) => (
+          <IconText
+            icon={icon}
+            text={text}
+            path={path}
+            key={index}
+            setActiveClass={setActiveClass}
+            activeClass={activeClass}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
