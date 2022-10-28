3e5c3f32dcc9a4402b5394e4e73d26a0da0ccb69
Rohit Gupta <lucidrohit@gmail.com>
2022-10-28 10:38:43 +0530

    added responsiveness in card items

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,60 +1,43 @@
-import "./cards.scss"
-import Card from "../common/card/card"
-import logo from "../../assets/imgs/carousel.webp"
+import "./cards.scss";
+import Card from "../common/card/card";
+import logo from "../../assets/imgs/carousel.webp";
 
 let cards = [
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-    {
-        img: logo,
-        head: 'Recoil',
-        info: 'The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....'
-    },
-
-]
-
+  {
+    img: logo,
+    head: "Recoil",
+    info: "The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....",
+  },
+  {
+    img: logo,
+    head: "Recoil",
+    info: "The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....",
+  },
+  {
+    img: logo,
+    head: "Recoil",
+    info: "The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....",
+  },
+  {
+    img: logo,
+    head: "Recoil",
+    info: "The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....",
+  },
+  {
+    img: logo,
+    head: "Recoil",
+    info: "The official media club of NIT Srinagar announces RECOIL- the eSports  tournament,  welcoming all thegamers to battle it out to be the ultimate champion.....",
+  },
+];
 
 function Cards() {
-    return (
-        <div className="cards">
-            {cards.map(
-                ({ img, head, info }, index) => (
-                    <Card
-                        key={index}
-                        img={img}
-                        head={head}
-                        info={info} />
-                ))}
-        </div>
-    )
+  return (
+    <div className="cards">
+      {cards.map(({ img, head, info }, index) => (
+        <Card key={index} img={img} head={head} info={info} />
+      ))}
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- src/components/common/card/card.jsx
+++ src/components/common/card/card.jsx
@@ -1,40 +1,31 @@
-import Button from '../button/button'
-import './card.scss'
+import Button from "../button/button";
+import "./card.scss";
 
-function Card({img, head, info}) {
-    return (
-        <div className="card">
-            <div className="card__img">
-                <img src={img} alt="" />
-            </div>
-            <div className="card__content">
-                <div className="content__date">
-                    <div className="date__month">
-                        June
-                    </div>
-                    <div className="date__day">
-                        11
-                    </div>
-                </div>
-                <div className="content__info">
-                    <h5 className="info__heading">
-                        {head}
-                    </h5>
-                    <p className="info__paragraph">
-                        {info}
-                    </p>
-                </div>
-            </div>
-                <div className="content__registration">
-                        <Button label={"Registerations Starts"}/>
-                    <div className="register__info">
-                        <p className="register__para">45 Registered</p>
-                        <p className="register__para">11 Days left.</p>
-                    </div>
-                </div>
+function Card({ img, head, info }) {
+  return (
+    <div className="card">
+      <div className="card__img">
+        <img src={img} alt="event" />
+      </div>
+      <div className="card__content">
+        <div className="content__date">
+          <div className="date__month">June</div>
+          <div className="date__day">11</div>
         </div>
-    )
+        <div className="content__info">
+          <h5 className="info__heading">{head}</h5>
+          <p className="info__paragraph">{info}</p>
+        </div>
+      </div>
+      <div className="content__registration">
+        <Button label={"Registerations Starts"} />
+        <div className="register__info">
+          <p className="register__para">45 Registered</p>
+          <p className="register__para">11 Days left.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default Card
+export default Card;
